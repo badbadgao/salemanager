dabef37d0d4c1a28ce6301c0f60d4025163a69d6
badbadgao <wave.gao2013@gmail.com>
2018-05-06 18:27:20 +1200

    configuration webapck

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         },
       },
       {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      },
+      {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
       },
