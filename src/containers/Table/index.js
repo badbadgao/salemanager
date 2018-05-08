27945ae1ec4bb27211cf914f50fe58d136c80e7c
badbadgao <wave.gao2013@gmail.com>
2018-05-08 20:41:46 +1200

    format package.json

---
+++ src/containers/Table/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
+
+class MVTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData: {}
+    }
+  }
+
+  render() {
+    return(
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn>Brand</TableHeaderColumn>
+            <TableHeaderColumn>Product Name</TableHeaderColumn>
+            <TableHeaderColumn>Quantity</TableHeaderColumn>
+            <TableHeaderColumn>Price</TableHeaderColumn>
+            <TableHeaderColumn>Total Price</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableRowColumn>Tomptson</TableRowColumn>
+            <TableRowColumn>Finsh Oil</TableRowColumn>
+            <TableRowColumn>10</TableRowColumn>
+            <TableRowColumn>$10.12</TableRowColumn>
+            <TableRowColumn>$101.2</TableRowColumn>
+          </TableRow>
+        </TableBody>
+      </Table>
+    )
+  }
+}
+
+export default MVTable;
